81f48f9e0c35aab7f33556bf9d81fb152166d369
stefanvasilev <stefanvasilev@halicea.com>
2026-04-06 17:48:57 +0200

    feat: add CreateUserRequest DTO for user registration

---
+++ wheelsync-backend/src/main/java/com/wheelsync/dto/user/CreateUserRequest.java
@@ -0,0 +1,37 @@
+package com.wheelsync.dto.user;
+
+import com.wheelsync.entity.enums.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateUserRequest {
+
+    @NotBlank(message = "First name is required")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    private String lastName;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters")
+    private String password;
+
+    private String phone;
+
+    private Role role;
+
+    private Long companyId;
+}
